f11a9b984ee5959b4914b9dc7746f0c65c3ccb4f
erinzellers <erinzellers@gmail.com>
2015-07-03 10:54:09 -0400

    added some bootstrap
    monkeying around with bootstrap and using it to supplement some of the
    css

--- main/js/main.js
+++ main/js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 		        dots: true,
 		        fade: true,
 		        adaptiveHeight: true,
-		        
+		        autoplay: true,
+  				autoplaySpeed: 2000,
 		        
 		      });
 		    });
